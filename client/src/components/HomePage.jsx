cf36e2373d9e37e32eba113388933d6f00f67ea2
Erik Grinn <grinn.erik@gmail.com>
2026-02-27 13:00:38 -0500

    checking auth and fetching users two async functions

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -3,45 +3,48 @@ import { useState, useEffect } from "react";
 // import styles from "../styles/App.module.css";
 
 function HomePage() {
+  const [fetchedData, setFetchedData] = useState([]);
+
   useEffect(() => {
     async function login() {
       const response = await fetch("http://localhost:8080/log-in", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include", // important for cookies/session
-        body: JSON.stringify({ username: "testtsfd", password: "fgdgfg" }),
+        body: JSON.stringify({ username: "new", password: "account" }),
       });
       const data = await response.json();
       console.log(data);
       return data;
     }
+    async function fetchData() {
+      try {
+        const response = await fetch("http://localhost:8080/");
+        let data = await response.json();
+        data = data.users;
+        console.log(data);
+        setFetchedData(data);
+        // axios
+        // const response = await axios.get("http://localhost:8080/");
+        // console.log(response.data);
+        // setFetchedData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    fetchData();
     login();
   }, []);
 
-  return <div>{"hello"}</div>;
+  return (
+    <div>
+      <ul>
+        {fetchedData.map((user, idx) => (
+          <li key={idx}>{user.username}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-//   const [fetchedData, setFetchedData] = useState("");
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       try {
-//         const response = await fetch("http://localhost:8080/");
-//         const data = await response.json();
-//         console.log(data.length);
-//         setFetchedData(data.length >= 1 ? data : "empty array");
-//         // axios
-//         // const response = await axios.get("http://localhost:8080/");
-//         // console.log(response.data);
-//         // setFetchedData(response.data);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     }
-//     fetchData();
-//   }, []);
-
-//   return <div>{fetchedData}</div>;
-// }
 
 export default HomePage;
